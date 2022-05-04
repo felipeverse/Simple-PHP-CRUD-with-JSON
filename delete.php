b7f884316e726ebd3cce817b04aaec4fd7bbb105
felipeverse <felipealvesrrodrigues@outlook.com>
2022-05-04 16:09:30 -0400

    delete using GET method

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/users/users.php';
+
+if (!isset($_GET['id'])) {
+    include "partials/not_found.php";
+    exit;
+}
+
+$userId = $_GET['id'];
+
+$user = getUserById($userId);
+if (!$user) {
+    include "partials/not_found.php";
+    exit;
+}
+
+deleteUser($userId);
+
+header('Location: index.php');
+?>
